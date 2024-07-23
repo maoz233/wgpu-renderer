c73b1f4387d973ef92c41b87d6a2c5cafb5ba3bc
MaoZ <mao.zhang233@gmail.com>
2024-07-23 00:50:59 -0500

    parse HDR header

--- src/hdr.ts
+++ src/hdr.ts
@@ -5,6 +5,7 @@ export default class HDRLoader {
 
   public async load(url: string): Promise<void> {
     this.buffer = await this.loadBuffer(url);
+    this.parseHDR();
   }
 
   private async loadBuffer(url: string): Promise<ArrayBuffer> {
@@ -16,5 +17,26 @@ export default class HDRLoader {
     const arrayBuffer = await res.arrayBuffer();
 
     return arrayBuffer;
+  }
+
+  private parseHDR(): void {
+    const bytes = new Uint8Array(this.buffer);
+    let header: string;
+    let i = 0;
+
+    while (i < bytes.length && bytes[i] !== 0x0a) {
+      header += String.fromCharCode(bytes[i]);
+      ++i;
+    }
+    header += "\n";
+
+    while (
+      i < bytes.length - 1 &&
+      (bytes[i] !== 0x0a || bytes[i + 1] !== 0x0a)
+    ) {
+      header += String.fromCharCode(bytes[i]);
+      ++i;
+    }
+    i += 2;
   }
 }
